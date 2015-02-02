e2f93a38d169f6e485f530c9125b9a42d4492e0a
YeongChai-07 <butchercai@gmail.com>
2015-02-02 13:31:33 +0800

    Solution to Part 3 Subpart 3 of the "Permission" app. Added "converter_activity.xml" for the UI implementations of the ConverterActivity. Added the dimension and strings values required by the UI implementations.

--- app/build/generated/source/r/debug/course/labs/permissionsapp/R.java
+++ app/build/generated/source/r/debug/course/labs/permissionsapp/R.java
@@ -14,12 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btn_Clear=0x7f060009;
+        public static final int btn_Convert=0x7f060008;
         public static final int get_bookmarks_button=0x7f060003;
         public static final int go_to_dangerous_activity_button=0x7f060004;
+        public static final int header_TemperatureApp=0x7f060005;
+        public static final int radioBtn_ToCelsius=0x7f060007;
+        public static final int radioBtn_ToFahrenheit=0x7f060006;
         public static final int start_bookmarks_button=0x7f060000;
         public static final int start_converter_button=0x7f060001;
-        public static final int start_dangerous_activity_button=0x7f060005;
+        public static final int start_dangerous_activity_button=0x7f06000b;
         public static final int text=0x7f060002;
+        public static final int tv_Result=0x7f06000a;
     }
     public static final class layout {
         public static final int activity_loader_activity=0x7f030000;
@@ -28,17 +34,23 @@ public final class R {
         public static final int go_to_dangerous_activity=0x7f030003;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int cust_button1=0x7f040001;
-        public static final int cust_message=0x7f040002;
-        public static final int main_button3=0x7f040003;
-        public static final int main_button4=0x7f040004;
-        public static final int perms_button1=0x7f040005;
-        public static final int perms_button2=0x7f040006;
-        public static final int place_holder=0x7f040007;
-        public static final int title_activity_customization=0x7f040008;
-        public static final int title_main=0x7f040009;
-        public static final int title_permissions=0x7f04000a;
+        public static final int TemperatureConverter_Header=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int btn_ClearTemperature=0x7f040002;
+        public static final int btn_ConvertTemperature=0x7f040003;
+        public static final int cust_button1=0x7f040004;
+        public static final int cust_message=0x7f040005;
+        public static final int editText_TemperatureToolTip=0x7f040006;
+        public static final int main_button3=0x7f040007;
+        public static final int main_button4=0x7f040008;
+        public static final int perms_button1=0x7f040009;
+        public static final int perms_button2=0x7f04000a;
+        public static final int place_holder=0x7f04000b;
+        public static final int radio_ToCelsius=0x7f04000c;
+        public static final int radio_ToFahrenheit=0x7f04000d;
+        public static final int title_activity_customization=0x7f04000e;
+        public static final int title_main=0x7f04000f;
+        public static final int title_permissions=0x7f040010;
     }
     public static final class style {
         /** 
