71299cbe0fddcd637b1dd703b1581860dfedf53e
YeongChai-07 <butchercai@gmail.com>
2015-02-03 20:03:29 +0800

    Solution to Part 3 Subpart 4 of the "Permission" app. Added code implementations of the temperature conversion logic of the "ConverterActivity". Removed unused import statements.

--- app/build/generated/source/r/debug/course/labs/permissionsapp/R.java
+++ app/build/generated/source/r/debug/course/labs/permissionsapp/R.java
@@ -10,22 +10,27 @@ package course.labs.permissionsapp;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int dimen_Density=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btn_Clear=0x7f060009;
-        public static final int btn_Convert=0x7f060008;
-        public static final int get_bookmarks_button=0x7f060003;
-        public static final int go_to_dangerous_activity_button=0x7f060004;
-        public static final int header_TemperatureApp=0x7f060005;
-        public static final int radioBtn_ToCelsius=0x7f060007;
-        public static final int radioBtn_ToFahrenheit=0x7f060006;
-        public static final int start_bookmarks_button=0x7f060000;
-        public static final int start_converter_button=0x7f060001;
-        public static final int start_dangerous_activity_button=0x7f06000b;
-        public static final int text=0x7f060002;
-        public static final int tv_Result=0x7f06000a;
+        public static final int btn_Clear=0x7f07000b;
+        public static final int btn_Convert=0x7f07000a;
+        public static final int editText_InputTemperature=0x7f070006;
+        public static final int get_bookmarks_button=0x7f070003;
+        public static final int go_to_dangerous_activity_button=0x7f070004;
+        public static final int header_TemperatureApp=0x7f070005;
+        public static final int radioBtn_ToCelsius=0x7f070009;
+        public static final int radioBtn_ToFahrenheit=0x7f070008;
+        public static final int radioGroup_ConversionChoice=0x7f070007;
+        public static final int start_bookmarks_button=0x7f070000;
+        public static final int start_converter_button=0x7f070001;
+        public static final int start_dangerous_activity_button=0x7f07000d;
+        public static final int text=0x7f070002;
+        public static final int tv_Result=0x7f07000c;
     }
     public static final class layout {
         public static final int activity_loader_activity=0x7f030000;
@@ -34,23 +39,23 @@ public final class R {
         public static final int go_to_dangerous_activity=0x7f030003;
     }
     public static final class string {
-        public static final int TemperatureConverter_Header=0x7f040000;
-        public static final int app_name=0x7f040001;
-        public static final int btn_ClearTemperature=0x7f040002;
-        public static final int btn_ConvertTemperature=0x7f040003;
-        public static final int cust_button1=0x7f040004;
-        public static final int cust_message=0x7f040005;
-        public static final int editText_TemperatureToolTip=0x7f040006;
-        public static final int main_button3=0x7f040007;
-        public static final int main_button4=0x7f040008;
-        public static final int perms_button1=0x7f040009;
-        public static final int perms_button2=0x7f04000a;
-        public static final int place_holder=0x7f04000b;
-        public static final int radio_ToCelsius=0x7f04000c;
-        public static final int radio_ToFahrenheit=0x7f04000d;
-        public static final int title_activity_customization=0x7f04000e;
-        public static final int title_main=0x7f04000f;
-        public static final int title_permissions=0x7f040010;
+        public static final int TemperatureConverter_Header=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int btn_ClearTemperature=0x7f050002;
+        public static final int btn_ConvertTemperature=0x7f050003;
+        public static final int cust_button1=0x7f050004;
+        public static final int cust_message=0x7f050005;
+        public static final int editText_TemperatureToolTip=0x7f050006;
+        public static final int main_button3=0x7f050007;
+        public static final int main_button4=0x7f050008;
+        public static final int perms_button1=0x7f050009;
+        public static final int perms_button2=0x7f05000a;
+        public static final int place_holder=0x7f05000b;
+        public static final int radio_ToCelsius=0x7f05000c;
+        public static final int radio_ToFahrenheit=0x7f05000d;
+        public static final int title_activity_customization=0x7f05000e;
+        public static final int title_main=0x7f05000f;
+        public static final int title_permissions=0x7f050010;
     }
     public static final class style {
         /** 
@@ -61,9 +66,9 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- app/src/main/java/course/labs/permissionsapp/ConverterActivity.java
+++ app/src/main/java/course/labs/permissionsapp/ConverterActivity.java
@@ -1,24 +1,132 @@
 package course.labs.permissionsapp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class ConverterActivity extends Activity {
 
     private static final String TAG = "Lab-Converter";
+    private static final int FREEZING_POINT_FAHRENHEIT = 32;   // A Constant temperature used for the conversion between Fahrenheit
+                                                              // because water freezes at this temperature (in Fahrenheit).
+
+    private static final int CELSIUS_RISING_SCALE = 100;        // Constant of the temperature rising scale for Celsius
+    private static final float FAHRENHEIT_RISING_SCALE = 180;        // Constant of the temperature rising scale for Celsius
+
+    private static final String CELSIUS_STATEMENT_CONST = " Celsius is ";        // Constant of the Celsius connective statement of the conversion result
+    private static final String FAHRENHEIT_STATEMENT_CONST = " Fahrenheit.";        // Constant of the Celsius connective statement of the conversion result
+
+    private static final float CONVERT_DECIMAL_POINTS = 10.0f;        // Constant used as a estimation unit involved by Multiplying and dividing a Floating-value
+                                                                      // to express it in 1 decimal place.
+
+    // Variables for reference to the UI Controls in the Activity
+    private RadioGroup mRadioGroup_Choice = null;
+    private TextView mTextView_Result = null;
+    private EditText mEditText_Temperature = null;
+    private Button mBtn_Convert = null;
+    private Button mBtn_Clear = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.converter_activity);
 
+        // Reference the UI Controls from the Layout
+        mTextView_Result = (TextView) findViewById(R.id.tv_Result);
+        mEditText_Temperature = (EditText)findViewById(R.id.editText_InputTemperature);
+        mBtn_Convert = (Button)findViewById(R.id.btn_Convert);
+        mBtn_Clear = (Button)findViewById(R.id.btn_Clear);
+        mRadioGroup_Choice = (RadioGroup)findViewById(R.id.radioGroup_ConversionChoice);
+
+        //Setting the OnClickListener for the Buttons
+        mBtn_Convert.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String inputString = mEditText_Temperature.getText().toString();        // Variable to store the input of the EditText
+
+                float convertedTemperature = 0.0f;        // Variable to store the converted temperature (either from Celsius or Fahrenheit)
+                boolean isInput_Fahrenheit = false;        // Variable used for keep track whether the input of a temperature is in Fahrenheit
+
+                if( ! (inputString.isEmpty()) )
+                {
+                    // Perform the temperature conversion when user input is not empty
+                    float in_Temperature = Float.parseFloat(inputString);
+
+                    if(mRadioGroup_Choice.getCheckedRadioButtonId() == R.id.radioBtn_ToFahrenheit)
+                    {
+                        //Process the conversion to Fahrenheit
+
+                        convertedTemperature = convertToFahrenheit(in_Temperature);
+                    }
+                    else if(mRadioGroup_Choice.getCheckedRadioButtonId() == R.id.radioBtn_ToCelsius)
+                    {
+                        //Process the conversion to Celsius
+                        isInput_Fahrenheit = true;
+                        convertedTemperature = convertToCelsius(in_Temperature);
+                    }
+
+                    // Give the converted temperature a closest estimate value.
+                    convertedTemperature = Math.round(convertedTemperature * CONVERT_DECIMAL_POINTS) / CONVERT_DECIMAL_POINTS;
+
+                    // Finally, display the result of the conversion
+                    displayResult(inputString, convertedTemperature, isInput_Fahrenheit);
+                }
+        }
+      }); // End of Convert button setOnClickListener function
+
+      mBtn_Clear.setOnClickListener(new OnClickListener(){
+
+          @Override
+          public void onClick(View v)
+          {
+
+              mEditText_Temperature.setText("");        // Clear the user's input from EditText.
+              mRadioGroup_Choice.check(R.id.radioBtn_ToFahrenheit);        // Assign "To Fahrenheit" Radio Button to be in Checked state.
+
+          }
+
+      }); // End of Clear button setOnClickListener function
+
     }
 
     // TODO - Implement the temperature conversion logic and other behavior here
+
+    private float convertToFahrenheit(float in_Celsius)
+    {
+        // Convert Celsius to Fahrenheit = (Celsius X 180/100) + 32
+        float toFahrenheit = in_Celsius * (FAHRENHEIT_RISING_SCALE / CELSIUS_RISING_SCALE );
+        toFahrenheit+= FREEZING_POINT_FAHRENHEIT;
+
+        return toFahrenheit;        // Returns the temperature value after conversion
+
+    }
+
+    private float convertToCelsius(float in_Fahrenheit)
+    {
+        // Convert Fahrenheit to Celsius = (Fahrenheit - 32) X 100/180
+        float toCelsius = in_Fahrenheit - FREEZING_POINT_FAHRENHEIT;
+        toCelsius *= (CELSIUS_RISING_SCALE / FAHRENHEIT_RISING_SCALE );
+
+        return toCelsius;        // Returns the temperature value after conversion
+    }
+
+    // Function for displaying the conversion result
+    private void displayResult(String inputTemperature, float convertedTemperature, boolean isInput_Fahrenheit)
+    {
+        if(isInput_Fahrenheit)
+        {
+            // Include the converted temperature (Celsius) in front of the output string
+            mTextView_Result.setText(convertedTemperature + CELSIUS_STATEMENT_CONST + inputTemperature + FAHRENHEIT_STATEMENT_CONST);
+        }
+
+        // Include the converted temperature (Fahrenheit) at the back of the output string
+        mTextView_Result.setText(inputTemperature + CELSIUS_STATEMENT_CONST + convertedTemperature + FAHRENHEIT_STATEMENT_CONST);
+    }
 
 }
